f064e08a6db3906d8d99d0681a362d703e833781
Piter <pitercapistrano@gmail.com>
2025-03-18 20:13:59 -0300

    Rotas Redefinidas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { AuthGuard } from './auth/auth.guard';
-
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'home', component: HomeComponent },
   // Rota para qualquer outra URL redireciona para login
   { path: '**', redirectTo: '/login' },
 ];
